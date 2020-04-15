df8c2704f3a3e226340727653dc2f1dcba66719d
hysro <306113292@qq.com>
2020-04-15 19:16:40 +0800

    新版的代码解析器

--- stellaris-config-parser/src/main/java/net/turanar/stellaris/domain/Category.java
+++ stellaris-config-parser/src/main/java/net/turanar/stellaris/domain/Category.java
@@ -4,11 +4,11 @@ import com.google.gson.annotations.SerializedName;
 
 public enum Category {
     // physics
-    粒子技术, @SerializedName("力场控制") 力场控制, 计算技术,
+    粒子物理, @SerializedName("力场控制") 力场操控, 计算技术,
     // society
-    生物科技, @SerializedName("军事理论") 军事理论, @SerializedName("殖民学说") 殖民学说, 治国之术, 心灵异能,
+    生物学, @SerializedName("军事理论") 军事理论, @SerializedName("殖民学说") 新世界理论, 治国术, 灵能理论,
     // engineering
-    工业技术, 材料科学, 推进技术, 太空技术;
+    工业技术, 材料科学, 推进力学, 宇航技术;
 
     public static Category eval(String name) {
 
